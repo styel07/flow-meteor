9b457f5a0c3aad026c067abc354d58657274beb6
styel07 <eleazarcorpuz@gmail.com>
2015-10-07 21:59:28 -1000

    signUp is working Sign in is not

--- client/view/login.js
+++ client/view/login.js
@@ -10,31 +10,31 @@
 
 // };
 
-Template.login.events({
-  'submit form' : function (e, t) {
-    e.preventDefault();
+// Template.login.events({
+//   'submit form' : function (e, t) {
+//     e.preventDefault();
 
-    username = $(e.target).find('#accounts-username').val(),
-    password = $(e.target).find('#accounts-password').val()
+//     username = $(e.target).find('#accounts-username').val(),
+//     password = $(e.target).find('#accounts-password').val()
 
-    Meteor.loginWithPassword(username, password, function(error) {
-      if (error) {
-        console.log('error', error);
-        throwError(error.reason);
-      } else {
-        if (Meteor.user().profile.role === "admin") {
-          // console.log("admin login");
-          Router.go("adminChoice");
-        } else if (Meteor.user().profile.role === "tester") {
-          console.log("tester login");
-          Router.go("agentInfo");
-        }
-      };
+//     Meteor.loginWithPassword(username, password, function(error) {
+//       if (error) {
+//         console.log('error', error);
+//         throwError(error.reason);
+//       } else {
+//         if (Meteor.user().profile.role === "admin") {
+//           // console.log("admin login");
+//           Router.go("adminChoice");
+//         } else if (Meteor.user().profile.role === "tester") {
+//           console.log("tester login");
+//           Router.go("agentInfo");
+//         }
+//       };
 
-    });
+//     });
 
-  }
-});
+//   }
+// });
 
 // Template.posts.helpers({
 //   postings : function(){
