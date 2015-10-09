b6b410dabf7e4f3e1f94dfa5e13b1a142b33686e
BraddahBoots <bryan@thebrocode.io>
2015-10-08 16:46:39 -1000

    created user profile

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 Meteor.startup(function () {
-  if (User.find().count() === 0) {
+  if (Tickets.find().count() === 0) {
     var names = [{title : "What is HTML?",
-                  body : "WTF is HTML anyways..."
+                  description : "WTF is HTML anyways..."
                  },
                  {title : "Why is the sky blue?",
-                  body : "I mean I get people THINK it's blue but who's to say it's not green"
+                  description : "I mean I get people THINK it's blue but who's to say it's not green"
                  },
                  {title : "Does anyone else hate simple get started tutorials?",
-                  body : "It seems like everyone of these starter tutorials makes it seems so easy and then I start to build something and can't figure anything out. Am I alone?"
+                  description : "It seems like everyone of these starter tutorials makes it seems so easy and then I start to build something and can't figure anything out. Am I alone?"
                  }
                 ];
     for (var i = 0; i < names.length; i++){
-      User.insert({title : names[i].title, body: names[i].body});
+      Tickets.insert({title : names[i].title, description: names[i].description});
     }
   }
 });
