9a0053f770291ad647f03f8b9829acfb16965fad
styel07 <eleazarcorpuz@gmail.com>
2015-10-12 11:52:06 -1000

    added styles to kanban

--- client/view/kanban/kanban.js
+++ client/view/kanban/kanban.js
@@ -32,7 +32,7 @@ Template.dashboard.events({
   'click #lessThan' : function() {
     // Checks if columnId is out of bound if it is not then decrement id by 1
     if (this.columnId !== 0) {
-      Tickets.update(this._id, { $inc : {columnId : -1}});
+      Tickets.update(this._id, { $inc : { columnId : -1}});
     }
   }
 });
@@ -41,7 +41,7 @@ Template.dashboard.events({
   'click #greaterThan' : function() {
     // Checks if columnId is out of bound if it is not then increment id by 1
     if (this.columnId !== 2) {
-      Tickets.update(this._id, { $inc : {columnId : 1}});
+      Tickets.update(this._id, { $inc : { columnId : 1}});
     }
   }
 });
